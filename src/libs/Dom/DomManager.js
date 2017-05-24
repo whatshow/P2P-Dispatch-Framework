b11eb9ae52376aedcc5d46016a1b0a4bba98fb7f
Dyn <chenda@witenet.com>
2017-05-24 11:04:08 +0800

    移除收集的重复资源，改进写法

--- src/libs/Dom/DomManager.js
+++ src/libs/Dom/DomManager.js
@@ -38,6 +38,8 @@
   var DOMManager = window.DOMManager = {};
   var resourceTypes = ['img', 'audio', 'video', 'source', 'track'];
   var defaultProp = 'ppdf-src';
+  var resources = [];
+  var nodes = [];
   var resourceMap = {
     'img': {definedProp: 'ppdf-src', prop: 'src'},
     'video': {definedProp: 'ppdf-src', prop: 'src'},
@@ -45,7 +47,6 @@
     'source': {definedProp: 'ppdf-src', prop: 'src'},// video or audio
     'track': {definedProp: 'ppdf-src', prop: 'src'},// video or audio
   };
-  var resourceNodes = [];
   var AUDIO = 'AUDIO';
   var VIDEO = 'VIDEO';
   var SOURCE = 'SOURCE';
@@ -58,28 +59,38 @@
   //TODO: audio vedio 资源加载研究
   //TODO: 视频音频浏览器支持情况检测
   //TODO: 内存释放研究
+  //TODO: video poster 支持
   DOMManager.collectPageResource = function (callback) {
-    var resources = resourceTypes
+    var rawData = resourceTypes
       .map(function (type) {
         return collectNodeResource(type);
       })
-      .filter(function (resource) {
-        return resource.length;
+      .filter(function (item) {
+        return !item.url;
       })
       .reduce(function (a, b) {
         return a.concat(b);
-      }, [])
-      .map(function (url) {
-        return {url: url};
-      });
+      }, []);
+
+    resources = rawData.filter(function (data, index, self) {
+      return self.findIndex(function (t) {
+          return t.url === data.url;
+        }) === index;
+    }).map(function (data) {
+      return {url: data.url};
+    })
+
+    nodes = rawData.map(function (data) {
+      return {type: data.type, node: data.node};
+    })
     console.log('收集的资源：', resources);
-    console.log('资源节点：', resourceNodes);
+    console.log('收集的节点：', nodes);
     typeof callback === 'function' && callback(resources);
     return resources;
   }
 
   DOMManager.onResourceLoadFailed = function (url) {
-    resourceNodes.forEach(function (item) {
+    nodes.forEach(function (item) {
       var node = item.node;
       var definedProp = resourceMap[item.type]['definedProp'];
       if (node.getAttribute(definedProp) === url) {
@@ -91,7 +102,7 @@
 
   DOMManager.addNodeResource = function (url, blob) {
     var objectURL = window.URL && window.URL.createObjectURL(blob);
-    resourceNodes.forEach(function (item) {
+    nodes.forEach(function (item) {
       console.log('添加节点资源：', url, '=>', objectURL);
       setNodeResource(item.node, item.type, url, objectURL);
     })
@@ -100,13 +111,7 @@
   function collectNodeResource(type) {
     return getNodes(type)
       .map(function (node) {
-        if (!checkNodeIsInList(node)) {
-          resourceNodes.push({node: node, type: type});
-        }
         return getURL(node, type);
-      })
-      .filter(function (url) {
-        return url;
       });
   }
 
@@ -122,11 +127,20 @@
   }
 
   function getURL(node, type) {
-    return isDOMNode(node) ? node.getAttribute(resourceMap[type]['definedProp'] || defaultProp) : '';
+    return isDOMNode(node) ?
+      {
+        url: node.getAttribute(resourceMap[type]['definedProp'] || defaultProp),
+        node: node,
+        type: type
+      } : {
+        node: node,
+        url: '',
+        type: type
+      };
   }
 
   function checkNodeIsInList(node) {
-    return resourceNodes.some(function (item) {
+    return nodes.some(function (item) {
       return isDOMNode(node) && node.isEqualNode(item.node)
     });
   }
@@ -181,22 +195,41 @@
    */
   var setButton = document.getElementById('set');
   var resetButton = document.getElementById('reset');
-  var testURL = './img.png';
+  var removeButton = document.getElementById('remove');
+
+  var testURL = ['./img.png', './video.mp4', './audio.mp3'];
+
   setButton.addEventListener('click', function (e) {
+    testURL.forEach(function (url) {
+      loadBlobResource(url);
+    })
+  })
+
+  resetButton.addEventListener('click', function (e) {
+    testURL.forEach(function (url) {
+      DOMManager.onResourceLoadFailed(url);
+    })
+  })
+
+  removeButton.addEventListener('click', function (e) {
+    nodes[0].node.remove();
+  })
+
+  Element.prototype.remove = function () {
+    this.parentElement.removeChild(this);
+  }
+
+  function loadBlobResource(url) {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', testURL, true);
+    xhr.open('GET', url, true);
     xhr.responseType = 'blob';
     xhr.onload = function (e) {
       if (this.status === 200) {
         var myBlob = this.response;
-        DOMManager.addNodeResource(testURL, myBlob);
+        DOMManager.addNodeResource(url, myBlob);
       }
     };
     xhr.send();
-  })
-
-  resetButton.addEventListener('click', function (e) {
-    DOMManager.onResourceLoadFailed(testURL);
-  })
+  }
 
 })()
